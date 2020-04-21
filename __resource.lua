a225ab18877db5eac09c92db3e7b0556efd15bd7
Redven-Biker <marween35620@gmail.com>
2020-04-21 17:29:12 +0200

    1.0

---
+++ __resource.lua
@@ -0,0 +1,37 @@
+files {
+    'index.html',
+    'music/loading.ogg',
+    'css/bootstrap.min.css',
+    'css/css.css',
+    'css/fontawesome-all.min.css',
+    'css/colors/yellow-2.css',
+    'css/style-dark.css',
+    'img/film-grain.png',
+    'img/logo-light.png',
+    'js/main.js',
+    'js/plugins.js',
+    'webfonts/fa-brands-400.eot',
+    'webfonts/fa-brands-400.svg',
+    'webfonts/fa-brands-400.ttf',
+    'webfonts/fa-brands-400.woff',
+    'webfonts/fa-brands-400.woff2',
+    'webfonts/fa-light-300.eot',
+    'webfonts/fa-light-300.svg',
+    'webfonts/fa-light-300.ttf',
+    'webfonts/fa-light-300.woff',
+    'webfonts/fa-light-300.woff2',
+    'webfonts/fa-regular-400.eot',
+    'webfonts/fa-regular-400.svg',
+    'webfonts/fa-regular-400.ttf',
+    'webfonts/fa-regular-400.woff',
+    'webfonts/fa-regular-400.woff2',
+    'webfonts/fa-solid-900.eot',
+    'webfonts/fa-solid-900.svg',
+    'webfonts/fa-solid-900.ttf',
+    'webfonts/fa-solid-900.woff',
+    'webfonts/fa-solid-900.woff2'
+}
+
+loadscreen 'index.html'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
